92a1ee980b7dc20316f7e9da55c3e4754942ca5c
Sunil Gurung <mrgurung2553@gmail.com>
2017-01-22 13:37:38 -0600

    initial add

--- src/com/sunil/DateFormater.java
+++ src/com/sunil/DateFormater.java
@@ -2,6 +2,7 @@ package com.sunil;
 
 public class DateFormater {
 	public static void main(String[] args) {
+		System.out.println("use of utility class");
 		System.out.println(DateUtility.getDate());
 	}
 }
